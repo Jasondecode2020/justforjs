a4e5e852dc6206122ccd6cc06de5b84faa498851
zhejie zhang <zzjasonzzj@gmail.com>
2021-09-09 02:05:00 +1000

    hello

---
+++ 1.py
@@ -0,0 +1,30 @@
+
+# n = int(sys.stdin.readline().strip())
+# print(n)
+# for i in range(n):
+#   line = sys.stdin.readline().strip()
+#   values = list(map(int, line.split()))
+#   print(values)
+
+
+# s = set()
+# s.add(1)
+# print(s)
+# pre = [1, 2, 4]
+# pre = pre[:-1]
+# print(pre)
+
+
+import sys
+# there is one number
+n = int(sys.stdin.readline().strip())
+print(n)
+# there are many numbers:
+line = sys.stdin.readline().strip()
+values = list(map(int, line.split()))
+print(values)
+# there are many lines
+for i in range(n):
+  line = sys.stdin.readline().strip()
+  values = list(map(int, line.split()))
+  print(values)
